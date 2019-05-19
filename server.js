496bbdc1b78b24d7a49c8c96783303585a3f396d
James Pham <jamespham93@yahoo.com>
2019-05-18 18:26:41 -0700

    add route to return json of client's header info

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+// allows us to get remote ip of user
+app.enable('trust proxy')
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,12 +24,19 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+// all information comes from header
+app.get('/api/whoami', (req, res) => {
+  const software = req.headers['user-agent']
+  const ipaddress = req.ip || req.ips
+  const language = req.headers['accept-language']
 
+  res.json({ ipaddress, language, software })
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000 || process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
